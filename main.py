dd234cd48244fb4fcc93f2c3eca2d74f4e02cf79
pejoma2 <pjoma2006@gmail.com>
2023-11-22 13:24:34 -0500

    agregando ciclo finito

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
 
+computer_wins = 0
+user_wins = 0
 rounds = 1
 
 while True:
@@ -11,13 +13,17 @@ while True:
     print("*" * 20)
     print("")
 
+    print("CPU WINS =>", computer_wins)
+    print("USER WINS =>", user_wins)
+
     options = ("piedra", "papel", "tijera")
 
     user_option = input("piedra, papel o tijera => ")
     user_option = user_option.lower()
 
     if not user_option in options:
-        print("No es una opció válida...")
+        print("No es una opción válida...")
+        continue
 
     computer_option = random.choice(options)
 
@@ -29,15 +35,32 @@ while True:
     elif user_option == "piedra":
         if computer_option == "tijera":
             print("PIEDRA gana a Tijera => User ganó")
+            user_wins += 1
         else:
             print("PAPEL gana a Piedra => CPU ganó")
+            computer_wins += 1
+
     elif user_option == "papel":
         if computer_option == "piedra":
             print("PAPEL gana a Piedra => User ganó")
+            user_wins += 1
         else:
             print("TIJERA gana a Papel => CPU ganó")
+            computer_wins += 1
     elif user_option == "tijera":
         if computer_option == "papel":
             print("TIJERA gana a Papel => User ganó")
+            user_wins += 1
         else:
             print("PIEDRA gana a Tijera => CPU ganó")
+            computer_wins += 1
+    
+    if computer_wins == 2:
+        print("🥳🥳El CPU ha ganado!🥳🥳")
+        break
+
+    if user_wins == 2:
+        print("🥳🥳El USER ha ganado!🥳🥳")
+        break
+
+    rounds += 1
